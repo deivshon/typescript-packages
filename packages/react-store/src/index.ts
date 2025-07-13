9f0e205b8b92c47708cd974b11691967965fe075
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2025-07-13 16:35:28 +0200

    refactor: ♻️ change `useWhole` export name

--- packages/react-store/src/index.ts
+++ packages/react-store/src/index.ts
@@ -1,3 +1,3 @@
-export { useStore, useWhole as useWholeStore } from "./hooks"
+export { useStore, useWhole } from "./hooks"
 export { shallowEq } from "./shallow"
 export { createStore } from "./store"
