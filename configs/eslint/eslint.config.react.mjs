89f189016794c047c1906fa7cb078731e8c4917e
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2025-08-14 18:54:50 +0200

    chore: 🔧 turn off `react/react-in-jsx-scope` rule in react eslint config

--- configs/eslint/eslint.config.react.mjs
+++ configs/eslint/eslint.config.react.mjs
@@ -23,6 +23,7 @@ export const reactEsLintConfig = tseslint.config(
             ...reactHooks.configs.recommended.rules,
             "react-hooks/rules-of-hooks": "error",
             "react-hooks/exhaustive-deps": "error",
+            "react/react-in-jsx-scope": "off",
         },
     },
 )
